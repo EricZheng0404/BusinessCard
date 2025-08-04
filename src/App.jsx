6c2da15df0e9c4c6f875f5bf88edec4709bc92de
Eric Zheng <erryzheng@gmail.com>
2025-08-04 16:22:06 -0400

    done with Footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
 import Main from './components/Main'
+import Footer from './components/Footer'
 function App() {
   return (
     <>
       <div className='container'>
         <Header />
         <Main />
+        <Footer />
       </div>
     </>
   )
---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+export default function Footer() {
+  return (
+    <div className="footer">
+      <div className="social-icon">
+        <i className="fab fa-twitter"></i>
+      </div>
+      <div className="social-icon">
+        <i className="fab fa-facebook"></i>
+      </div>
+      <div className="social-icon">
+        <i className="fab fa-instagram"></i>
+      </div>
+      <div className="social-icon">
+        <i className="fab fa-github"></i>
+      </div>
+    </div>
+  )
+}
