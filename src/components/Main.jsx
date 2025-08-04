088a3a74186b8c694c978e56e8a12fc42e13bf6f
Eric Zheng <erryzheng@gmail.com>
2025-08-04 16:18:53 -0400

    done with Main

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+export default function Main() {
+  return (
+    <div className="main">
+      <div className="about">
+        <h3>About</h3>
+        <p>I am a full stack developer with a passion for building web applications. I am a quick learner and I am always looking to expand my knowledge.</p>
+      </div>
+      <div className="interests">
+        <h3>Interests</h3>
+        <p>I am a full stack developer with a passion for building web applications. I am a quick learner and I am always looking to expand my knowledge.</p>
+      </div>
+    </div>
+  )
+}
