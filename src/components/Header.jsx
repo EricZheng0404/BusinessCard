0721eebbf014f0c6a8cb465542863f337d88bfc0
Eric Zheng <erryzheng@gmail.com>
2025-08-04 16:11:22 -0400

    done with Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,22 @@
+import profile from '../assets/profile.png'
 export default function Header() {
   return (
     <div>
-      
+      <img src={profile} alt="profile" />
+      <div className="name">
+        <h1>Eric Zheng</h1>
+        <h2>Full Stack Developer</h2>
+      </div>
+      <div className="contacts">
+        <button className="email-btn">
+          <i className="fas fa-envelope"></i>
+          Email
+        </button>
+        <button className="linkedin-btn">
+          <i className="fab fa-linkedin"></i>
+          LinkedIn
+        </button>
+      </div>
     </div>
   )
 }
